9db0daec384dd6b184793cebc5e7842737b81707
Vincent Handoyo <vincenthan28@gmail.com>
2026-02-11 00:48:54 +0700

    update catalog admin

--- backend/catalog/admin.py
+++ backend/catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Dataset, AccessRequest
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Dataset)
+admin.site.register(AccessRequest)
